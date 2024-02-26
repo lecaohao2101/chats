3892d06e98b94207493177280aeac515d7e757c2
Cao Hào Lê <lecaohao2101@gmail.com>
2024-02-27 04:41:18 +0700

    UI-1

--- main.py
+++ main.py
@@ -9,16 +9,18 @@ socketio = SocketIO(app)
 
 rooms = {}
 
+
 def generate_unique_code(length):
     while True:
         code = ""
         for _ in range(length):
             code += random.choice(ascii_uppercase)
-        
+
         if code not in rooms:
             break
-    
+
     return code
+
 
 @app.route("/", methods=["POST", "GET"])
 def home():
@@ -34,19 +36,20 @@ def home():
 
         if join != False and not code:
             return render_template("home.html", error="Please enter a room code.", code=code, name=name)
-        
+
         room = code
         if create != False:
             room = generate_unique_code(4)
             rooms[room] = {"members": 0, "messages": []}
         elif code not in rooms:
             return render_template("home.html", error="Room does not exist.", code=code, name=name)
-        
+
         session["room"] = room
         session["name"] = name
         return redirect(url_for("room"))
 
     return render_template("home.html")
+
 
 @app.route("/room")
 def room():
@@ -56,12 +59,13 @@ def room():
 
     return render_template("room.html", code=room, messages=rooms[room]["messages"])
 
+
 # @socketio.on("message")
 # def message(data):
 #     room = session.get("room")
 #     if room not in rooms:
 #         return 
-    
+
 #     content = {
 #         "name": session.get("name"),
 #         "message": data["data"]
@@ -84,6 +88,7 @@ def message(data):
     rooms[room]["messages"].append(content)
     print(f"{session.get('name')} said: {data['message']}")
 
+
 @socketio.on("connect")
 def connect(auth):
     room = session.get("room")
@@ -93,11 +98,12 @@ def connect(auth):
     if room not in rooms:
         leave_room(room)
         return
-    
+
     join_room(room)
     send({"name": name, "message": "has entered the room"}, to=room)
     rooms[room]["members"] += 1
     print(f"{name} joined room {room}")
+
 
 @socketio.on("disconnect")
 def disconnect():
@@ -109,9 +115,10 @@ def disconnect():
         rooms[room]["members"] -= 1
         if rooms[room]["members"] <= 0:
             del rooms[room]
-    
+
     send({"name": name, "message": "has left the room"}, to=room)
     print(f"{name} has left the room {room}")
+
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
